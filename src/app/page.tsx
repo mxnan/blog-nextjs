b45ba30209821c5dc425140ddaa6078a4d3a3fb6
mxnan <128995981+mxnan@users.noreply.github.com>
2024-05-22 22:48:00 +0530

    :tada:
    :white_check_mark: setup nextjs app with shadcn components ,dark mode enabled, tailwind setup .
    :white_check_mark: added navbar and dark mode swticher .
    :white_check_mark: updated ui/button ,secondary and outline to re-use .

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section>
+      <Button variant={"secondary"}>button</Button>
+    </section>
+  );
+}
---
+++ src/components/nav/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ToggleTheme from "../theme-toggle";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { VscGithubAlt } from "react-icons/vsc";
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between">
+      <ToggleTheme />
+      <div className="flex items-center gap-3">
+        <Link href="/">Home</Link>
+        <Button variant={"outline"} className="flex text-sm items-center gap-2">
+          <VscGithubAlt strokeWidth={1.5} />
+          Login
+        </Button>
+      </div>
+    </nav>
+  );
+}
