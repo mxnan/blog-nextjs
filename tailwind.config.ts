52acb2326cc5e542348dff0ffb91e98c187eec9d
mxnan <128995981+mxnan@users.noreply.github.com>
2024-06-08 11:52:23 +0530

    :art:
    :white_check_mark:  changed light and dark colors , and changed colors from theme-toggle

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,8 +15,8 @@ const config = {
         xs: "480px",
       },
       colors: {
-        lightmode: "#f97316",
-        darkmode: "#0ea5e9",
+        lightmode: "#64b5f6",
+        darkmode: "#5829a7",
       },
 
       fontFamily: {
